814c7295b8d979e7300b7d029e1c8d422edc7b90
araujoajoao <araujoaojoao@gmail.com>
2022-04-30 09:26:43 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleMarketList::Application.config.secret_token = 'caccbab1eb01db45942499acbd9a91f73d01b19f96a30384af01bcc615107b8c801bfa96307594fabf961148320ce0aa28bdaedbb46901544492b478b5ee36e0'
